0a9247e32c26e24f9663a660ec1c03d5179cf7cc
Feargal Egan <egan.feargal.web@gmail.com>
2023-07-27 12:58:14 +0100

    Added a button and passphrase for a bit of experimentation

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        passphrase = request.form.get('passphrase')
+        if not passphrase:
+            error_msg = 'Please enter Passphrase!'
+            return render_template('index.html', error_msg=error_msg)
+        else:
+            return render_template('index.html', passphrase=passphrase)
+    return render_template('templates/index.html')
