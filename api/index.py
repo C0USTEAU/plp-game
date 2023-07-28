0f542c7c89fa5dd773d472994e5d078fdd33dfe2
Feargal Egan <egan.feargal.web@gmail.com>
2023-07-28 11:29:37 +0100

    Added root page and a start template accessed by a link

--- api/index.py
+++ api/index.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/')
+def root():
+    return render_template('root.html')
+
+@app.route('/start', methods=['GET', 'POST'])
+def start():
     if request.method == 'POST':
         passphrase = request.form.get('passphrase')
         if not passphrase:
             error_msg = 'Please enter Passphrase!'
-            return render_template('index.html', error_msg=error_msg)
+            return render_template('start.html', error_msg=error_msg)
         else:
-            return render_template('index.html', passphrase=passphrase)
-    return render_template('templates/index.html')
+            return render_template('start.html', passphrase=passphrase)
+    return render_template('start.html')
