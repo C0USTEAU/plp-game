08d06470da1a325d57590c2d25e4cbe90d53e31c
Feargal Egan <egan.feargal.web@gmail.com>
2023-07-31 08:03:46 +0100

    Another attempt to figure out how to do a linebreak, plus first stab at a dropdown for selecting gameweek fixtures

--- api/index.py
+++ api/index.py
@@ -3,6 +3,19 @@ import logging
 
 app = Flask(__name__)
 
+fixtures = [
+    (1, 'Burnley', 'Man City', '2023-08-11T19:00:00Z'),
+    (1, 'Arsenal', "Nott'm Forest", '2023-08-12T11:30:00Z'),
+    (2, 'Bournemouth', 'West Ham', '2023-08-12T14:00:00Z'),
+    (2, 'Brighton', 'Luton', '2023-08-12T14:00:00Z'),
+    (3, 'Everton', 'Fulham', '2023-08-12T14:00:00Z'),
+    (3, 'Sheffield Utd', 'Crystal Palace', '2023-08-12T14:00:00Z'),
+    (4, 'Newcastle', 'Aston Villa', '2023-08-12T16:30:00Z'),
+    (4, 'Brentford', 'Spurs', '2023-08-13T13:00:00Z'),
+    (5, 'Chelsea', 'Liverpool', '2023-08-13T15:30:00Z'),
+    (5, 'Man Utd', 'Wolves', '2023-08-14T19:00:00Z')
+]
+
 @app.route('/')
 @app.route('/dashboard')
 def dashboard():
@@ -10,7 +23,11 @@ def dashboard():
 
 @app.route('/gameweeks', methods=['GET', 'POST'])
 def gameweeks():
-    return render_template('gameweeks.html')
+    if request.method == 'POST':
+        selected_gameweek = int(request.form.get('gameweek'))
+        selected_fixtures = [f for f in fixtures if f[0] == selected_gameweek]
+        return render_template('gameweeks.html', fixtures=selected_fixtures)
+    return render_template('gameweeks.html', fixtures=None)
 
 @app.route('/start', methods=['GET', 'POST'])
 def start():
